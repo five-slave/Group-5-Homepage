245f6594c30c9ea54240794213eea8f8cd3d48c7
jinu <jjw8967@ajou.ac.kr>
2019-07-03 16:53:45 +0900

    error handling file create

--- lotto.c
+++ lotto.c
@@ -4,8 +4,12 @@
 int save_log(int *num){
 
 
-	FILE *fp = fopen("log.txt","a");
-	
+	FILE *fp = fopen("log.txt","a");	
+	if(fp==NULL){
+		perror("failed create file log.txt ");
+		return 0;
+	}
+
 	fclose(fp);
 
 	return 1;
